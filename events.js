d9bf5fe1fb4348a0127991c9a0cf2d8a1891829c
Pasanteweb <Pasanteweb@adm.urbe.edu>
2023-06-30 16:05:55 -0400

    Añadidos: Garage, Obstaculo, Dificultad y musica

--- events.js
+++ events.js
@@ -1,9 +1,49 @@
-//Juego de carro vertical esquivando huecos
-//(cada hueco que llegue al fondo de la pantalla son +10 puntos)
+//Juego de carro vertical agarrando huecos
+//(cada hueco que se agarre son +100 puntos)
+
+//HACER ESCAPE PARA VOLVER A TITLE SCREEN
+
+document.onload = () => {};
 
 const canvas = document.getElementById("myCanvas");
 
 const ctx = canvas.getContext("2d");
+
+const titleScreen = document.getElementById("title-screen");
+const instructions = document.getElementById("how-to");
+const carSelect = document.getElementById("carousel");
+
+function goSelect() {
+  titleScreen.style.display = "none";
+  instructions.style.display = "none";
+  carSelect.style.display = "block";
+  garageMusic.play();
+}
+
+function startGame(car) {
+  carSelect.style.display = "none";
+  garageMusic.pause();
+  console.log(car);
+  //alert("The car is: " + car)
+  Carro.src = car;
+  // Display the game canvas
+  canvas.style.display = "block";
+  bgMusic.play();
+}
+
+//Sonidos
+const gameOver = new Audio("explosion.wav");
+//const engineStart = new Audio("car-start.wav");
+const holeFall = new Audio("holefall.wav");
+const bgMusic = new Audio("bg-music.mp3");
+const garageMusic = new Audio("garage-music.mp3");
+bgMusic.currentTime = 3; // audio at perfect point
+
+//CARROS DEL GARAGE
+let redCar = "redcar.png";
+let altheus = "A.png";
+let matiz = "matiz.png"
+//AÑADIR CARROS
 
 //Carro
 let xCarro = canvas.width / 4;
@@ -15,7 +55,6 @@ let acceleration = -0.1; // constant acceleration
 let steeringAngle = 0; // initial steering angle
 let maxSteeringAngle = Math.PI / 6; // maximum steering angle in radians
 const Carro = new Image();
-Carro.src = "A.png";
 
 //Pause
 let pause = true;
@@ -25,10 +64,11 @@ document.addEventListener("keydown", function (event) {
   if (event.key === "p" || event.key === "P") {
     // Toggle the paused variable
     pause = !pause;
+    start = null;
   }
 });
 
-//Obstaculo
+//Hueco para puntuar (uyqrico)
 //DIFICULTAD; AUMENTAR TAMAÑO DEL HUECO O VELOCIDAD
 let holeWidth = 70;
 let holeHeight = 70;
@@ -38,17 +78,30 @@ let yHole = -70;
 const Hole = new Image();
 Hole.src = "hole.png";
 
-//Sonidos
-const gameOver = new Audio("explosion.wav");
-//const engineStart = new Audio("car-start.wav");
-const holeFall = new Audio("holefall.wav");
-const bgMusic = new Audio("bg-music.mp3");
+//Poste de luz (obstaculo)
+let poleWidth = 80;
+let poleHeight = 70;
+let poleSpeed = 6;
+let xPost = Math.random() * (canvas.width - poleWidth + 5);
+let yPost = -80;
+const Pole = new Image();
+Pole.src = "pole.png";
 
 //PAUSA
 function drawPause() {
   ctx.font = "16px Baskerville";
   ctx.fillStyle = "#000";
   ctx.fillText("PAUSED GAME", 50, canvas.height / 2);
+}
+
+//Press Start
+
+let start = true;
+
+function drawStart() {
+  ctx.font = "16px Baskerville";
+  ctx.fillStyle = "#000";
+  ctx.fillText("PRESS P TO START ROLLING", 70, canvas.height / 2);
 }
 
 //RESETEA EL JUEGO
@@ -68,10 +121,17 @@ function resetGame() {
   xHole = Math.random() * (canvas.width - holeWidth);
   yHole = -70;
 
+  poleWidth = 70;
+  poleHeight = 70;
+  poleSpeed = 5;
+  xPost = Math.random() * (canvas.width - holeWidth);
+  yPost = -70;
+
   score = 0;
 
-  bgMusic.currentTime = 0; // reset audio to start
-  bgMusic.play();}
+  bgMusic.currentTime = 3; // reset audio to start
+  bgMusic.play();
+}
 
 //Aceleracion y frenado
 function vroom() {
@@ -81,20 +141,25 @@ function vroom() {
     ySpeed = 0;
     acceleration = -0.1;
     yCarro = canvas.height - carHeight;
-    alert("Chocaste! CUTUPLUN!")
-    resetGame()
+    alert("Chocaste! CUTUPLUN!");
+    resetGame();
   } else if (yCarro < 0) {
     ySpeed = 0; // reset speed and acceleration when car hits top
     acceleration = -0.1;
     yCarro = 0;
     bgMusic.pause();
-    gameOver.play();    
+    gameOver.play();
     alert("GAME OVER, you crashed PIMPUMPAM");
     resetGame();
   }
   xCarro += ySpeed * Math.tan(steeringAngle); // update x position based on steering angle and y speed
   xCarro = Math.max(xCarro, 0); // prevent car from going off the left edge of the canvas
   xCarro = Math.min(xCarro, canvas.width - carWidth); // prevent car from going off the right edge of the canvas
+
+  stripePosition -= ySpeed; // update stripe position based on car speed
+  if (stripePosition < -40) {
+    stripePosition += 40; // wrap around when the stripes go off the top of the canvas
+  }
 }
 
 document.addEventListener("keydown", (event) => {
@@ -121,107 +186,187 @@ document.addEventListener("keyup", (event) => {
 
 //Score
 let score = 0;
-//Chequea si el hueco llegó al final de la pantalla
-let holeReachedBottom = false;
+//Chequea si el hueco tocó el carro
+let holeTouched = false;
+//Setea +100 de score
+let setScore = false;
 
 function drawScore() {
-  if (holeReachedBottom) {
-    ctx.font = "40px, Baskerville";
-    ctx.fillStyle = "#ff000";
+  if (holeTouched) {
+    ctx.font = "45px, Baskerville";
+    ctx.fillStyle = "#fafafa";
     ctx.fillText("+100", 20, 20);
     setTimeout(() => {
       ctx.clearRect(20, 20, 50, 35); // Clear previous +100 string
+      holeTouched = false;
     }, 1000);
   }
-  ctx.font = "40px, Baskerville";
-  ctx.fillStyle = "#ff000";
+  ctx.font = "45px, Baskerville";
+  ctx.fillStyle = "#fff";
   ctx.fillText("Score: " + score, 150, 30);
+}
+
+//Rayas de la calle
+
+let stripePosition = 0;
+
+function drawStripes() {
+  ctx.fillStyle = "#fff"; // set the fill color to white
+  for (let i = stripePosition; i < canvas.height; i += 40) {
+    ctx.fillRect(canvas.width / 2 - 5, i, 10, 20); // draw a white rectangle
+  }
+  ctx.fillStyle = "#bdbf24"; // set the fill color to red
+  for (let i = stripePosition; i < canvas.height; i += 120) {
+    ctx.fillRect(canvas.width / 2 - 5, i, 10, 60); // draw a red rectangle
+  }
 }
 
 function holeSpawn() {
   //xHole = Math.random() * (canvas.width-holeWidth)
   yHole += holeSpeed;
   if (yHole > canvas.height - holeHeight) {
-    if (!holeReachedBottom) {
-      score += 100;
-      holeReachedBottom = true;
-      drawScore();
-      console.log("Should draw score");
-      console.log(holeReachedBottom); // reset flag
-    }
     yHole = -holeHeight;
     xHole = Math.random() * (canvas.width - holeWidth);
     if (score > 500) {
-      holeWidth = 120;
-      holeSpeed = 7
+      holeWidth = 50;
+      holeSpeed = 7;
     }
-    holeReachedBottom = false;
-    console.log(holeReachedBottom);
-    console.log("Should reset"); // reset flag
+    //holeTouched = false;
+    //console.log(holeTouched);
+    //console.log("Should reset"); // reset flag
+  }
+}
+
+function poleSpawn() {
+  //xHole = Math.random() * (canvas.width-holeWidth)
+  yPost += poleSpeed;
+  if (yPost > canvas.height - poleHeight) {
+    yPost = -poleHeight;
+    xPost = Math.random() * (canvas.width - holeWidth);
+    if (score > 500) {
+      poleWidth = 90;
+      poleSpeed = 4;
+    }
+    //holeTouched = false;
+    //console.log(holeTouched);
+    //console.log("Should reset"); // reset flag
+  }
+}
+
+function detectPole() {
+  if (
+    score > 500 &&
+    xCarro < xPost + poleWidth / 1.5 &&
+    xCarro + carWidth / 2 > xPost &&
+    yCarro < yPost + poleHeight / 2 &&
+    yCarro + carHeight / 2 > yPost
+  ) {
+    bgMusic.pause();
+    holeFall.play();
+    alert("CRASHHHH!!!! Quedaste a pie :C");
+    resetGame();
+  } else if (
+    xCarro < xPost + poleWidth / 2 &&
+    xCarro + carWidth / 2 > xPost &&
+    yCarro < yPost + poleHeight / 2 &&
+    yCarro + carHeight / 2 > yPost
+  ) {
+    bgMusic.pause();
+    holeFall.play();
+    alert("CRASHHHH!!!! Quedaste a pie :C");
+    resetGame();
   }
 }
 
 function detectHole() {
+  if (
+    score > 500 &&
+    xCarro < xHole + holeWidth / 1.5 &&
+    xCarro + carWidth / 2 > xHole &&
+    yCarro < yHole + holeHeight / 2 &&
+    yCarro + carHeight / 2 > yHole
+  ) {
+    if (!setScore) {
+      score += 100;
+      setScore = true;
+      holeTouched = true;
+      drawScore();
+      yHole = -holeHeight;
+      xHole = Math.random() * (canvas.width - holeWidth);
+    }
 
-  if(score > 500 && xCarro < xHole + holeWidth / 1.5 &&
-    xCarro + carWidth/2 > xHole &&
+    console.log("Should draw score");
+    setScore = false;
+    //console.log(holeTouched);
+  } else if (
+    xCarro < xHole + holeWidth / 2 &&
+    xCarro + carWidth / 2 > xHole &&
     yCarro < yHole + holeHeight / 2 &&
-    yCarro + carHeight/2 > yHole){
-   
-    holeFall.play();
-    bgMusic.pause();
-    alert("Caiste en un hueco, colabora pa los frescos");
-    resetGame();
-  }  else if(xCarro < xHole + holeWidth / 2 &&
-    xCarro + carWidth/2 > xHole &&
-    yCarro < yHole + holeHeight / 2 &&
-    yCarro + carHeight/2 > yHole) {
-    holeFall.play();
-    bgMusic.pause();
-    alert("Caiste en un hueco, colabora pa los frescos");
-    resetGame();
+    yCarro + carHeight / 2 > yHole
+  ) {
+    if (!setScore) {
+      score += 100;
+      setScore = true;
+      yHole = -holeHeight;
+      xHole = Math.random() * (canvas.width - holeWidth);
+    }
+    holeTouched = true;
+    drawScore();
+    console.log("Should draw score");
+    setScore = false;
+    //console.log(holeTouched);
   }
 }
 
-const titleScreen = document.getElementById("title-screen");
-
-function startGame() {
-  titleScreen.style.display = "none";
-
-  // Display the game canvas
-  canvas.style.display = "block";
-  bgMusic.play();
-}
-
+//GAMELOOP
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  if (pause) {
+  if (pause === start) {
+    drawStart();
+    bgMusic.pause();
+  } else if (pause) {
     drawPause();
     bgMusic.pause();
   } else {
+    bgMusic.play();
+
     Carro.onload = () => {
       console.log("aqui hay un carro");
     };
 
     Hole.onload = () => {
-      console.log("obstacle loaded");
+      console.log("objective loaded");
     };
-    ctx.drawImage(Carro, xCarro, yCarro, carWidth, carHeight);
+    Pole.onload = () => {
+      console.log("Obstacle loaded");
+    };
 
+    stripePosition -= ySpeed; // update stripe position based on car speed
+    if (stripePosition < -40) {
+      stripePosition += 40; // wrap around when the stripes go off the top of the canvas
+    }
+
+    drawStripes();
+
+    ctx.drawImage(Carro, xCarro, yCarro, carWidth, carHeight);
 
     ctx.drawImage(Hole, xHole, yHole, holeWidth, holeHeight);
 
+    ctx.drawImage(Pole, xPost, yPost, poleWidth, poleHeight);
+
+    poleSpawn();
     holeSpawn();
     drawScore();
 
     vroom();
 
+    detectPole();
     detectHole();
 
-    if (!bgMusic.paused) {
-      bgMusic.play();
-    }
+    //if (!bgMusic.paused) {
+    //  bgMusic.play();
+    //}
   }
   requestAnimationFrame(draw);
 }
